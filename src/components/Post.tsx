fafc10738f84c02d96b4035fc6ed60a3ada5ba4a
Matias Monteagudo <me92monteagudo@gmail.com>
2025-10-31 02:16:27 -0300

    Modificaciones para cambiar diseño de galeria en modo detallado, controles y caja de comentarios.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -37,12 +37,13 @@ interface PostImage {
 
 interface PostProps {
     post: PostType;
+    detailed?: boolean;
 }
 
-export default function Post({ post }: PostProps){
+export default function Post({ post, detailed = false }: PostProps){
     const [postImgs, setPostImgs] = useState<PostImage[]>([]);
     const [imgMain, setImgMain] = useState<number>(0);
-    const [showCommentBox, setShowCommentBox] = useState<boolean>(false);
+    const [showCommentBox, setShowCommentBox] = useState<boolean>(detailed);
     const navigate = useNavigate();
 
     const getImages = async () => {
@@ -74,25 +75,44 @@ export default function Post({ post }: PostProps){
             </div>
             <div className="mt-2">{ post.description }</div>
             {/* Imagenes */}
-            <div className="mt-2 d-flex flex-column flex-md-row" style={{ display: Array.isArray(postImgs) && postImgs.length > 0 ? 'flex' : 'none' }}>
-                <img
-                    src={postImgs[imgMain]?.url}
-                    className='border border-secondary rounded img-fluid mb-2 mb-md-0 me-md-2'
-                    style={{ flex: 1, height: '400px', objectFit: 'cover' }}
-                />
-                <div className='d-flex gap-2 flex-row flex-md-column' style={{ maxHeight: '400px', scrollbarWidth: 'thin', overflowY: 'auto', overflowX: 'auto' }}>
-                    {
-                        postImgs.map((img, i) => (
-                            <img
-                                src={img.url}
-                                key={i} className={`border rounded ${i === imgMain ? 'border-3 border-primary' : 'border-secondary'}`}
-                                style={{ width: '150px', height: '150px', objectFit: 'cover', flexShrink: 0 }}
-                                onClick={() => setImgMain(i)}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
+            { 
+                !detailed ?
+                    <div className="mt-2 d-flex flex-column flex-md-row" style={{ display: Array.isArray(postImgs) && postImgs.length > 0 ? 'flex' : 'none' }}>
+                        <img
+                            src={postImgs[imgMain]?.url}
+                            className='border border-secondary p-1 rounded img-fluid mb-2 mb-md-0 me-md-2'
+                            style={{ flex: 1, height: '500px', objectFit: 'cover' }}
+                        />
+                        <div className='d-flex gap-2 flex-row flex-md-column' style={{ maxHeight: '500px', scrollbarWidth: 'thin', overflowY: 'auto', overflowX: 'auto' }}>
+                            {
+                                postImgs.map((img, i) => (
+                                    <img
+                                        src={img.url}
+                                        key={i}
+                                        className={`border rounded p-1 ${i === imgMain ? 'border-3 border-primary' : 'border-secondary'}`}
+                                        style={{ width: '200px', height: '200px', objectFit: 'cover', flexShrink: 0 }}
+                                        onClick={() => setImgMain(i)}
+                                    />
+                                ))
+                            }
+                        </div>
+                    </div>
+                :
+                    <div className='d-flex justify-content-center position-relative w-100 py-2'>
+                        <button className='btn border-0 position-absolute top-50 start-0 translate-middle-y fs-1 ms-3' disabled={imgMain == 0} onClick={() => setImgMain(imgMain - 1)}>
+                            <i className='bi bi-arrow-left-circle-fill text-gray' />
+                        </button>
+                        <img
+                            src={postImgs[imgMain]?.url}
+                            className={'w-100 p-1 border rounded border-secondary'}
+                            style={{ objectFit: 'contain', maxHeight: '70dvh' }}
+                        />
+                        <button className='btn border-0 position-absolute top-50 end-0 translate-middle-y fs-1 me-3' disabled={imgMain == postImgs.length -1} onClick={() => setImgMain(imgMain + 1)}>
+                            <i className='bi bi-arrow-right-circle-fill text-gray' />
+                        </button>
+                    </div>
+            }
+
             {/* Etiquetas */}
             <div className='mt-2 d-inline-block text-muted'>
                 <b>Etiquetas:</b>
@@ -103,7 +123,7 @@ export default function Post({ post }: PostProps){
                 }
             </div>
             {/* Acciones */}
-            <div className='container-fluid d-flex justify-content-between mt-2'>
+            <div className={`container-fluid justify-content-between mt-2 ${detailed ? 'd-none' : 'd-flex'}`}>
                 <button className="btn w-100 mx-1" onClick={() => setShowCommentBox(!showCommentBox)}>
                     <i className="bi bi-chat me-1"/>Comentar
                 </button>
@@ -111,6 +131,9 @@ export default function Post({ post }: PostProps){
                     <i className="bi bi-box-arrow-up-right me-1"/>Ver más
                 </button>
             </div>
+            <button className={`btn w-100 mx-1 ${detailed ? '' : 'd-none'}`} onClick={() => navigate(-1)}>
+                <i className="bi bi-arrow-left-circle me-1"/>Volver atrás
+            </button>
             {/* Comentarios */}
             <PostComments post_id={post.id} show_comment_box={showCommentBox} />
         </div>
